1c3fb169cb112c4ddeb01fdcab9ff37d3b93f555
Tomas Vazquez <vazquezt2018@gmail.com>
2025-02-08 20:14:37 -0300

    Initialize Django blog application with models, views, templates, and configuration files

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from blog.models import Category, Post, Comment
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+class PostAdmin(admin.ModelAdmin):
+    pass
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
