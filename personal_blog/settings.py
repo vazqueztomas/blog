06fe6f9f45082cc38ff82f2dc48cb21fd43f5aa4
Tomas Vazquez <vazquezt2018@gmail.com>
2025-02-09 20:38:39 -0300

    Update static file settings in Django and Vercel configuration

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -101,16 +101,9 @@ print("Database Configuration:", DATABASES['default'])
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATIC_URL = '/static/'
-
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
