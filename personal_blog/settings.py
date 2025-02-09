cf2c97b115c7807a8d04972ab7b594ad66089f79
Tomas Vazquez <vazquezt2018@gmail.com>
2025-02-09 12:21:26 -0300

    Update database name environment variable in Django settings

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -90,7 +90,7 @@ STORAGES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('PGDATABASE'),
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get('POSTGRES_HOST'),
