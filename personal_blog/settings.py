a272628057c2348e2855e87115fb32e6f9ece24d
Tomas Vazquez <vazquezt2018@gmail.com>
2025-02-09 22:20:51 -0300

    Set DEBUG to True for local development

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b(t!t9l()gjr$_^^$0h-12e6@e3xyl8tuhz@k%wab2*4xxstgl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", "tomasblog.vercel.app"]
 
