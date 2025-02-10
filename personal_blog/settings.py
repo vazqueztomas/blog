7630a84d5cf2a57c429cfe8d390ecc3da3457d8d
Tomas Vazquez <vazquezt2018@gmail.com>
2025-02-09 22:27:15 -0300

    Add decouple for improved environment variable management

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+from decouple import config
 import dj_database_url
 
 load_dotenv()
